d0441acc11e83f1a2cc3b1fa6282cb5d5ea1b95f
Bruno Paulino <bruno@woombikes.com>
2020-01-24 12:12:56 +0100

    props

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const user = {
+    firstName: 'Jill',
+    lastName: 'Koh'
+  };
+
   return (
     <div className="App">
-      <HelloWorld/>
+      <HelloWorld user={user}/>
     </div>
   );
 }
 
-function HelloWorld() {
+function HelloWorld(props) {
+  const {firstName, lastName} = props.user;
   return(
     <div>
       <h1>Hello, world!</h1>
+      <h2>My first name is {`${firstName} ${lastName}`}</h2>
     </div>
   );
 }

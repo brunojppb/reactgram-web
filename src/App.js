63f2f7877efde71650b8f6b8a8b1789f31d5c332
Bruno Paulino <bruno@woombikes.com>
2020-01-26 13:31:28 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  const user = {
-    firstName: 'Jill',
-    lastName: 'Koh'
+  const onButtonClick = (event) => {
+    setCount(current => current + 1);
   };
 
   return (
     <div className="App">
-      <HelloWorld user={user}/>
-    </div>
-  );
-}
-
-function HelloWorld(props) {
-  const {firstName, lastName} = props.user;
-  return(
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>My first name is {`${firstName} ${lastName}`}</h2>
+      <h2>Current Count: {count}</h2>
+      <button onClick={onButtonClick}>Increment Counter</button>
     </div>
   );
 }

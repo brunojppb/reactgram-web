14df7cce244a1cdccba5d1009cd2072c49f012f6
Bruno Paulino <bruno@woombikes.com>
2020-02-23 12:37:27 +0100

    Creating the about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import AboutPage from './components/static/AboutPage';
 
 function App() {
 
   return (
     <div className="App">
-      <AboutPage/>
+      <h1>Reactgram</h1>
+      <Router>
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route path="/about">
+            <AboutPage/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/static/AboutPage.js
+++ src/components/static/AboutPage.js
@@ -3,9 +3,10 @@ import React from 'react';
 function AboutPage() {
 
   return(
-    <h1>
-      About Page
-    </h1>
+    <div>
+      <h1>About Page</h1>
+      <p>React is the most popular frontend library on the market. Built by Facebook, it is based on functional components which are updated automatically when a state update occurs. Designed around a declarative architecture, components created in React are much easier to build, debug and maintain.</p>
+    </div>
   )
 
 }

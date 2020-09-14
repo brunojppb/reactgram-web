d36ade43f4c16c514ba90545e2298218a42d6a86
Bruno Paulino <bruno@woombikes.com>
2020-09-14 21:50:37 +0200

    add prettier support and enable VS Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import AboutPage from "./components/static/AboutPage";
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import AboutPage from './components/static/AboutPage'
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         </Switch>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/static/AboutPage.js
+++ src/components/static/AboutPage.js
@@ -1,12 +1,11 @@
-import React from "react";
-import aboutSvg from "../../img/about.svg";
-import '../../style/static/main.css';
+import React from 'react'
+import aboutSvg from '../../img/about.svg'
+import '../../style/static/main.css'
 
 function AboutPage() {
-
   return (
     <div className="static-page-wrapper">
-      <img src={aboutSvg} alt="about" className="static-page-img"/>
+      <img src={aboutSvg} alt="about" className="static-page-img" />
       <h1>About Page</h1>
       <p>
         React is the most popular frontend library on the market. Built by
@@ -16,7 +15,7 @@ function AboutPage() {
         debug and maintain.
       </p>
     </div>
-  );
+  )
 }
 
-export default AboutPage;
+export default AboutPage
